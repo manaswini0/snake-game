5ee85ba1116e63fbdc967dea2ce267642f59c420
bmanaswini <manaswin@buffalo.edu>
2020-08-09 18:56:50 -0400

    sound addition

--- game.py
+++ game.py
@@ -1,6 +1,6 @@
 import pygame
 import random
-
+import os
 import snake_class
 import obs_class
 
@@ -10,19 +10,44 @@ win = pygame.display.set_mode((500, 500))
 
 pygame.display.set_caption("Snake-game")
 
-all_sprites = pygame.sprite.Group()
-snake = snake_class.Snake(50, 50)
-all_sprites.add(snake)
+h=20
+w=30
+
+snake_sprites = pygame.sprite.Group()
+obs_sprites=pygame.sprite.Group()
+snake = snake_class.Snake(50, 50,h,w)
+snake_sprites.add(snake)
+
+
 #obstacles
 def obstacles():
 
     for i in range(0,20):
         obs=obs_class.Obs(random.randrange(500),random.randrange(500))
-        all_sprites.add(obs)
+        obs_sprites.add(obs)
 
 
 obstacles()
 
+def collisions():
+
+    for o in pygame.sprite.spritecollide(snake, obs_sprites, 1):
+
+        pygame.sprite.Sprite.remove(o)
+        os.system("afplay snakeatt.mp3&")
+        # pygame.sprite.Sprite.remove(snake)
+        increase_len()
+        # h=h+5
+        # w=w+5
+
+
+
+def increase_len():
+
+
+
+    # snake_sprites.add(snake)
+    print ("kk")
 run = True
 
 
@@ -31,11 +56,14 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run=False
-    all_sprites.update()
+    snake_sprites.update()
 
     win.fill((0,0,0))
     #draws all the sprites on screen
-    all_sprites.draw(win)
+    snake.update()
+    collisions()
+    snake_sprites.draw(win)
+    obs_sprites.draw(win)
     pygame.time.delay(25)
     pygame.display.update()
 
--- snake_class.py
+++ snake_class.py
@@ -1,14 +1,14 @@
 import pygame
 
 
+
 class Snake(pygame.sprite.Sprite):
-    def __init__(self, x, y):
+    def __init__(self, x, y, h,w):
         # Call the parent class (Sprite) constructor
         pygame.sprite.Sprite.__init__(self)
 
-        self.image = pygame.Surface((20,50))
+        self.image = pygame.Surface((h,w))
         self.image.fill((255,0,0))
-
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
     def update(self):
